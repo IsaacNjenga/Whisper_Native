f00cc64d8f34eba3a663a3dcaea933ae20fa835d
Isaac Njenga <ayzzoh20@gmail.com>
2025-07-29 16:30:29 +0300

    backend update

---
+++ back/controllers/userController.js
@@ -0,0 +1,89 @@
+import { StreamChat } from "stream-chat";
+import UserModel from "../models/User.js";
+
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+
+const client = StreamChat.getInstance(api_key, api_secret);
+
+const fetchUser = async (req, res) => {
+  const { id } = req.query;
+  if (!id) return res.status(404).json({ message: "No ID specified" });
+  try {
+    const user = await UserModel.findById(id);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    const { _id, email, avatar } = user;
+    return res
+      .status(200)
+      .json({ success: true, userDetails: { _id, email, avatar } });
+  } catch (error) {
+    console.log("Error on user fetch", error);
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { id } = req.query;
+  if (!id) return res.status(404).json({ message: "No ID specified" });
+  const { email, avatar } = req.body;
+
+  const updates = {};
+  if (email) updates.email = email;
+  if (avatar) updates.avatar = avatar;
+
+  try {
+    const user = await UserModel.findByIdAndUpdate(
+      { _id: id },
+      { $set: updates },
+      { new: true }
+    );
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    const updatedUser = await client.upsertUser({
+      id,
+      email: email || "noemail@example.com",
+      image: avatar || null,
+    });
+    return res.status(200).json({
+      success: true,
+      user: {
+        _id: updatedUser._id,
+        avatar: updatedUser.image,
+        email: updatedUser.email,
+      },
+    });
+  } catch (error) {
+    console.log("Error on user update", error);
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const { id } = req.query;
+  if (!id) return res.status(404).json({ message: "No ID specified" });
+  try {
+    await UserModel.findByIdAndDelete({ _id: id });
+    await client.deleteUser(id, {
+      delete_conversation_channels: true,
+      mark_messages_deleted: true,
+      hard_delete: true,
+    });
+    return res
+      .status(200)
+      .json({ success: true, message: "Successfully deleted!" });
+  } catch (error) {
+    console.log("Error on user delete", error);
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+export { fetchUser, deleteUser, updateUser };
--- back/routes/routes.js
+++ back/routes/routes.js
@@ -1,9 +1,20 @@
 import express from "express";
 import { Register, Login } from "../controllers/authController.js";
+import {
+  updateUser,
+  fetchUser,
+  deleteUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
+//auth routes
 router.post("/sign-up", Register);
 router.post("/sign-in", Login);
+
+//user routes
+router.put("/update-user", updateUser);
+router.get("/fetch-user", fetchUser);
+router.delete("/delete-user", deleteUser);
 
 export { router as Router };
